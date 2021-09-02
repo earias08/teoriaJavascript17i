e60e5aa88ef71d5e5ae2b30e304c3b08adc76864
Emilse <emilsearias@gmail.com>
2021-09-02 18:42:47 -0300

    se agrego el archivo de funciones con funciones sin parametros y con parametros

--- js/6-funciones.js
+++ js/6-funciones.js
@@ -6,5 +6,16 @@ function aumentarPrecio(){
     document.write('Valor total del servicio disney plus $'+ (disney + inflacion));
 };
 
+// funcion con parametros
+function actualizarPrecio(monto, producto, precio){
+    document.write('<br>El producto: '+producto+' precio total $'+ (precio +monto))
+}
+
 // invocar o llamar a una funcion
 aumentarPrecio();
+
+let coeficienteInflacion = parseInt(prompt('Ingrese el monto total de la inflacion del mes'));
+
+actualizarPrecio(coeficienteInflacion, 'Teclado hyperx ',9000);
+actualizarPrecio(coeficienteInflacion, 'Teclado Logitec ',14000);
+actualizarPrecio('mouse logitec', 9000 , 100);
